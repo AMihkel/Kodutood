deee3d75f23c578d08cf5fcc4d63e5234280c324
AMihkel <andre.mihkel3@gmail.com>
2024-05-17 19:51:08 +0300

    valmis kodutöö

--- Kodutoo_nr_3/teenused.php
+++ Kodutoo_nr_3/teenused.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Kodutöö</title>
+    <title>Kodutöö nr 3</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
@@ -83,14 +83,31 @@
             </table>
         </form>
         <?php
-          if ((!empty($_GET["nimi"])) && (!empty($_GET["teenus"]) && (!empty($_GET["aste"])))) {
+          if ((isset($_GET["nimi"])) && (!empty($_GET["teenus"]) && (!empty($_GET["aste"])))) {
 
             $nimi = $_GET['nimi'];
             $teenus = $_GET['teenus'];
             $aste = $_GET['aste'];
+            $maksumus = $teenus*$aste;
+            if ($teenus==10) {
+              $t_nimi="ühe ruumi puhastus";
+            }
+            elseif ($teenus==50) {
+              $t_nimi="korteri puhastus";
+            }
+            elseif ($teenus==100) {
+              $t_nimi="maja puhastus";
+            }
+            echo '<br><h2>Tere '.$nimi.', sinu '.$t_nimi.' läheb maksma '.$maksumus.' eurot!! </h2>';
+            
+          
     }
-    elseif ((!empty($_GET["nimi"])) && (empty($_GET["teenus"]))) {
-      echo "Palun vali ka Teenus";
+    elseif ((isset($_GET["nimi"])) && (empty($_GET["teenus"]))) {
+      ?>
+      <div class=" bg-danger text-center">
+  Palun vali mis teenust soovid!!
+</div>
+      <?php
     }
        
     ?>
