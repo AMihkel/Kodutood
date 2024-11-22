e55ccbfe39cff77f9a5b5c3a336d2d4ff5ee83ed
AMihkel <andre.mihkel3@gmail.com>
2024-11-22 19:03:32 +0200

    see windowsis töötab
    Proovin kas see töötab ka linuxis

--- Kodutoo_nr_6/config.php
+++ Kodutoo_nr_6/config.php
@@ -4,7 +4,7 @@ $dbserver = "localhost";
 $andmepaas = "andmed";
 $pw = "Par00l123";
 
-$yhendus = "mysqli_connect($dbserver,$kasutaja,$pw,$andmepaas);";
+$yhendus = mysqli_connect($dbserver,$kasutaja,$pw,$andmepaas);
 if (!$yhendus) {
     die ("jälle ebaõnnestus!!");
 }
--- Kodutoo_nr_6/index.php
+++ Kodutoo_nr_6/index.php
@@ -1,30 +1,33 @@
-<?php
-$kasutaja = "miku";   // Your MySQL username
-$pw = "Par00l123";     // Your MySQL password
-$dbserver = "localhost";  // The MySQL server (localhost in your case)
-$andmepaas = "andmed";  // The name of your database
+<?php include("config.php"); ?>
+<?php include("header.php"); ?>
+<div class="container">
+        <h1>Parimad albumid!!</h1>
+        
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <form action="" method="get">
+            Otsi: <input type="text" name="s">
+            <input type="submit" value="otsi">
+        </form>
+          <?php 
+            $paring = " SELECT `asset_name`, `location`
+                        FROM `network_inventory` 
+                        ORDER BY `asset_name` ASC
+                        LIMIT 10;
+                        ";
+            $valjund = mysqli_query($yhendus, $paring);
+            if ($valjund) {
+              while ($rida = mysqli_fetch_assoc($valjund)) {
+                  // Echo individual fields
+                  echo "Asset Name: " . $rida['asset_name'];
+                  echo "Location: " . $rida['location'] . "<br><br>";
+              }
+          } else {
+              echo "Error: " . mysqli_error($yhendus);
+          }
+          ?>
+        </div>
+</div>
 
-// Establish the connection
-$yhendus = mysqli_connect($dbserver, $kasutaja, $pw, $andmepaas);
 
-// Check if the connection is successful
-if (!$yhendus) {
-    die("Connection failed: " . mysqli_connect_error());
-}
 
-$paring = "SELECT `asset_name`, `location`
-            FROM `network_inventory` 
-            ORDER BY `asset_name` ASC
-            LIMIT 10";
-
-$valjund = mysqli_query($yhendus, $paring);
-
-if ($valjund) {
-    while ($rida = mysqli_fetch_assoc($valjund)) {
-        echo "Asset Name: " . $rida['asset_name'] . "<br>";
-        echo "Location: " . $rida['location'] . "<br><br>";
-    }
-} else {
-    echo "Error: " . mysqli_error($yhendus);
-}
-?>
+<?php include("footer.php"); ?>
