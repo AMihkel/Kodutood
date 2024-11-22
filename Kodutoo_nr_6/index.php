d2bfdfe1640ec49b4defd0a9e17af268f88ce89e
AMihkel <andre.mihkel3@gmail.com>
2024-11-22 23:10:42 +0200

    Valmis

--- Kodutoo_nr_6/index.php
+++ Kodutoo_nr_6/index.php
@@ -1,33 +1,121 @@
 <?php include("config.php"); ?>
 <?php include("header.php"); ?>
-<div class="container">
-        <h1>Parimad albumid!!</h1>
+<div class="container-fluid">
         
-        <div class="row row-cols-1 row-cols-md-3 g-4">
-          <form action="" method="get">
-            Otsi: <input type="text" name="s">
-            <input type="submit" value="otsi">
-        </form>
-          <?php 
-            $paring = " SELECT `asset_name`, `location`
-                        FROM `network_inventory` 
-                        ORDER BY `asset_name` ASC
-                        LIMIT 10;
-                        ";
-            $valjund = mysqli_query($yhendus, $paring);
-            if ($valjund) {
-              while ($rida = mysqli_fetch_assoc($valjund)) {
-                  // Echo individual fields
-                  echo "Asset Name: " . $rida['asset_name'];
-                  echo "Location: " . $rida['location'] . "<br><br>";
-              }
-          } else {
-              echo "Error: " . mysqli_error($yhendus);
+           <?php
+        //p2ringud andmebaasi
+             //Vali kÃµikide seadmete nimed, IP-aadressid ja asukoht ning kuva 10 esimest
+            $paring1 = ' SELECT `asset_name`,`ip_address`,`location` FROM `network_inventory` LIMIT 10;';
+            //Kuva kolm kÃµige vanemat seadet
+            $paring2 = " SELECT `asset_name`,`purchase_date` FROM `network_inventory` ORDER BY `purchase_date` LIMIT 3;";
+            //Kuva kÃµikide seadmete arv, mis on praegu hoolduses
+            $paring3 = " SELECT COUNT(*) FROM `network_inventory` WHERE `status` = 'maintenance';";
+            //Leia, mitu seadet on igas asukohas
+            $paring4 = "SELECT `location`, COUNT(*) AS `device_count` FROM `network_inventory` GROUP BY `location` ORDER BY `device_count` DESC LIMIT 15;";
+        
+            
+            //saadan soovitud ühendusele minu päringu
+            
+            //vastused
+            ?>
+            <!-- Vali kÃµikide seadmete nimed, IP-aadressid ja asukoht ning kuva 10 esimest -->
+            <div class="container-fluid">
+            <caption><strong>IP aadressid ja asukohad</strong></caption>
+              <table class="table table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">Nimi</th>
+                    <th scope="col">IP-aadress</th>
+                    <th scope="col">Asukoht</th>
+                  </tr>
+                </thead>
+                <?php
+                $valjund = mysqli_query($yhendus, $paring1);
+            while ($rida  = mysqli_fetch_assoc($valjund)) {
+              echo '
+                <tbody>
+                  <tr>
+                    <td>'.$rida['asset_name'].'</td>
+                    <td>'.$rida['ip_address'].'</td>
+                    <td>'.$rida['location'].'</td>
+                  </tr>
+                  
+                ';
+
           }
-          ?>
-        </div>
+            ?> 
+            </tbody>
+              </table>
+            </div>
+            <!-- Kuva kolm kÃµige vanemat seadet -->
+            <div class="container-fluid">
+            <caption><strong>Kõige vanemad masinad</strong></caption>
+              <table class="table table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">Nimi</th>
+                    <th scope="col">Ostmise kuupäev</th>
+                  </tr>
+                </thead>
+                <?php
+                $valjund = mysqli_query($yhendus, $paring2);
+            while ($rida  = mysqli_fetch_assoc($valjund)) {
+              //print_r ($rida);
+              echo '
+                <tbody>
+                  <tr>
+                    <td>'.$rida['asset_name'].'</td>
+                    <td>'.$rida['purchase_date'].'</td>
+                  </tr>
+                  
+                ';
+
+          }
+            ?> 
+            </tbody>
+              </table>
+
+              <!-- Kuva kÃµikide seadmete arv, mis on praegu hoolduses -->
+              <?php 
+              $valjund = mysqli_query($yhendus, $paring3);
+              if ($valjund) {
+                $rida = mysqli_fetch_assoc($valjund);
+                //print_r($rida);
+                $nr = $rida['COUNT(*)']; // COUNT(*) on mysql-i enda muutuja kui kasutada lugemist
+                echo "Hetkel on " . $nr . " seadet paranduses";
+            }
+              ?>
+            <!-- Leia, mitu seadet on igas asukohas, panin limiidiks 15 -->
+            <div class="container-fluid">
+            <caption><strong>Mitu seadet on asukohas</strong></caption>
+              <table class="table table-sm">
+                <thead>
+                  <tr>
+                    <th scope="col">Asukoht</th>
+                    <th scope="col">Mitu tk</th>
+                  </tr>
+                </thead>
+                <?php
+                $valjund = mysqli_query($yhendus, $paring4);
+            while ($rida  = mysqli_fetch_assoc($valjund)) {
+              //print_r ($rida);
+              echo '
+                <tbody>
+                  <tr>
+                    <td>'.$rida['location'].'</td>
+                    <td>'.$rida['device_count'].'</td>
+                  </tr>
+                  
+                ';
+
+          }
+            ?> 
+            </tbody>
+              </table>
+
+              
+            </div>
 </div>
-
-
+        
 
 <?php include("footer.php"); ?>
