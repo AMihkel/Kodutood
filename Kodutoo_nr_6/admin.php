71eb8ce85401f7c7659a29a79813f6350d8faf53
AMihkel <andre.mihkel3@gmail.com>
2024-11-22 17:05:43 +0200

    algus

---
+++ Kodutoo_nr_6/admin.php
@@ -0,0 +1,7 @@
+<?php
+//sinu andmed
+$db_server = 'localhost';
+$db_andmebaas = 'andmed';
+$db_kasutaja = 'miku';
+$db_salasona = 'Par00l123';
+?>
---
+++ Kodutoo_nr_6/index.php
@@ -0,0 +1,11 @@
+<?php include("header.php"); ?>
+
+
+<div class="container">
+    <h4 class="text-center py-4">Parimad pakkumised</h4>
+    <div class="row">
+    </div>
+</div>
+<?php
+?>
+<?php include("footer.php"); ?>
